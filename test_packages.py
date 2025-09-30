cb545fc93dd307289dc903e3f85d8bd6e12112cf
UnzEva <uevgenya18@gmail.com>
2025-09-29 20:01:40 -0400

    Setup: Project structure

---
+++ test_packages.py
@@ -0,0 +1,11 @@
+try:
+    import numpy
+    import pandas
+    import matplotlib
+    import plotly
+    import seaborn
+    import dash
+    import streamlit
+    print("✅ OK!")
+except ImportError as e:
+    print(f"Error: {e}")
